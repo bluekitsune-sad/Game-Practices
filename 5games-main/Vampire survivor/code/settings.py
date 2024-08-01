5aa0f788c79a36dcc9ee0086a4757e829272ccf6
BlueKitsune狐 <116144344+bluekitsune-sad@users.noreply.github.com>
2024-08-01 22:37:47 +0500

    -_-

--- 5games-main/Vampire survivor/code/settings.py
+++ 5games-main/Vampire survivor/code/settings.py
@@ -1,6 +1,8 @@
-import pygame 
-from os.path import join 
+import pygame
+from os.path import join
 from os import walk
+import sys
+from pygame import *
 
-WINDOW_WIDTH, WINDOW_HEIGHT = 1280,720 
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 TILE_SIZE = 64
